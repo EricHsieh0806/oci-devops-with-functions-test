c869f6b9af5e07ef4903b73ce271086de81ca78c
EricHsieh0806 <30400643+EricHsieh0806@users.noreply.github.com>
2022-12-13 14:42:21 +0800

    Update tf

--- providers.tf
+++ providers.tf
@@ -25,23 +25,18 @@ terraform {
 }
 
 provider "oci" {
-            tenancy-ocid = var.tenancy-ocid
-            region       = var.region
-
-            }
-
-provider "oci" {
-            alias        = "home-region"
-            tenancy-ocid = var.tenancy-ocid
-            region       = lookup(data.oci-identity-regions.home-region.regions[0], "name")
-
-
-            }
+  tenancy_ocid = var.tenancy_ocid
+  region       = var.region
+}
 
 provider "oci" {
-            alias        = "current-region"
-            tenancy-ocid = var.tenancy-ocid
-            region       = var.region
+  alias        = "home_region"
+  tenancy_ocid = var.tenancy_ocid
+  region       = lookup(data.oci_identity_regions.home_region.regions[0], "name")
+}
 
-
-            }
+provider "oci" {
+  alias        = "current_region"
+  tenancy_ocid = var.tenancy_ocid
+  region       = var.region
+}
